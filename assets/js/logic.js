2a534646b0ca821311c91c234d5805b705692788
Nick Sasseen <40394298+NickSasseen@users.noreply.github.com>
2020-05-21 17:36:13 -0400

    jjj

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -64,21 +64,47 @@ $(document).ready(function () {
 
 
     $('#btnStart').click(function () {
+        var total = 0;
 
         $('#gameInstructions').hide();
         if (whosThere == 'Mom') {
             $('.mom').show();
+            total = $('.mom').find('.card:visible').length;
         } else {
             $('.rick').show();
+            total = $('.rick').find('.card:visible').length;
         }
+
+        $('.js-count').show();
+        $('#complete').html(0);
+        $('#total').html(total);
     });
 
     $('.js-found').click(function() {
         var parent = $(this).data('card');
         $(parent).remove();
+        var done = false;
 
-        if ($('.js-found:visible').length == 0) {
-            console.log('congrats');
+        if (whosThere == 'Mom') {
+            done = $('.mom').find('.card:visible').length == 0;
+        } else {
+            done = $('.rick').find('.card:visible').length == 0;
+        }
+
+        if (done) {
+            $('#done').show();
+            $('.js-count').hide();
+        } else {
+            $('#complete').html(function() {
+                var total = $('#total').html();
+                var complete = 0;
+
+                if (whosThere == 'Mom') {
+                    return total - $('.mom').find('.card:visible').length;
+                } else {
+                    return total - $('.rick').find('.card:visible').length;
+                }
+            });
         }
     });
 });
